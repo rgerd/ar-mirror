2fc8b5c2d5f956af939c76e2b86378efbf07a4df
Robert Gerdisch <robert.gerdisch@yale.edu>
2018-11-15 16:45:47 -0500

    Overengineering

--- camera_capture.py
+++ camera_capture.py
@@ -8,11 +8,14 @@
 
 import numpy as np
 import cv2 as cv
+from face import Face
 
 cap = cv.VideoCapture(0)
 
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
+
+faces = []
 
 while(True):
     # Capture frame-by-frame
@@ -20,19 +23,32 @@ while(True):
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-    if frame is None or gray is None:
-        continue
+    possible_faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
+    if len(possible_faces) == 0:
+        del faces[:]
+
+    for face in faces:
+        detected_face_index = face.update_face(gray, possible_faces)
+
+        if detected_face_index != -1:
+            del possible_faces[detected_face_index]
+
+    for new_face in possible_faces:
+        faces.append(Face())
+        faces[-1].set_face(new_face)
+
+    for updated_face in faces:
+        (x, y, w, h) = updated_face.bounding_rect
         cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
         
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
+        # roi_gray = gray[y:y+int(h * 0.6), x:x+w]
+        # roi_color = frame[y:y+h, x:x+w]
         
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex,ey,ew,eh) in eyes:
-            cv.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0,255,0), 2)
+        # new_eyes = eye_cascade.detectMultiScale(roi_gray)
+
+        # for (ex,ey,ew,eh) in eyes:
+        #     cv.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0,255,0), 2)
 
     # Display the resulting frame
     cv.imshow('frame', frame)
