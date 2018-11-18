a36dc339cef24e64bbc1209b515b33c460f37182
Robert Gerdisch <robert.gerdisch@yale.edu>
2018-11-18 14:17:37 -0500

    Tweaked comments

--- scripts/face_dataset.py
+++ scripts/face_dataset.py
@@ -4,7 +4,6 @@ cam = cv2.VideoCapture(0)
 
 face_detector = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
-# For each person, enter one numeric face id
 face_id = input('\n enter user id end press <return> ==>  ')
 print("\n [INFO] Initializing face capture. Look the camera and wait ...")
 
@@ -23,14 +22,14 @@ while(True):
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
         count += 1
 
-        # Save the captured image into the datasets folder
+        # Save the captured image to the datasets folder
         cv2.imwrite("dataset/" + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
         cv2.imshow('image', img)
 
-    k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+    k = cv2.waitKey(100) & 0xff # Press 'ESC' to exit data collection
     if k == 27:
         break
-    elif count >= 120: # Take 120 face sample and stop video
+    elif count >= 120: # Take 120 face samples and stop video
          break
 
 # Do a bit of cleanup
--- scripts/face_training.py
+++ scripts/face_training.py
@@ -2,30 +2,29 @@ import cv2
 import numpy as np
 from PIL import Image
 import os
-# Path for face image database
-path = 'data/dataset'
+
+data_path = 'data/dataset'
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 detector = cv2.CascadeClassifier("data/haarcascade_frontalface_default.xml");
-# function to get the images and label data
-def getImagesAndLabels(path):
-    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
+
+def getImagesAndLabels(data_path):
+    imagePaths = [os.data_path.join(data_path,f) for f in os.listdir(data_path)]
     faceSamples=[]
     ids = []
     for imagePath in imagePaths:
-        if os.path.split(imagePath)[-1][0] == '.':
+        if os.data_path.split(imagePath)[-1][0] == '.':
             continue
-        PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
+        PIL_img = Image.open(imagePath).convert('L') # convert to grayscale
         img_numpy = np.array(PIL_img,'uint8')
-        id = int(os.path.split(imagePath)[-1].split(".")[0])
+        id = int(os.data_path.split(imagePath)[-1].split(".")[0])
         faces = detector.detectMultiScale(img_numpy)
         for (x,y,w,h) in faces:
             faceSamples.append(img_numpy[y:y+h,x:x+w])
             ids.append(id)
     return faceSamples,ids
 print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
-faces,ids = getImagesAndLabels(path)
+faces,ids = getImagesAndLabels(data_path)
 recognizer.train(faces, np.array(ids))
-# Save the model into trainer/trainer.yml
-recognizer.write('trainer/trainer.yml') # recognizer.save() worked on Mac, but not on Pi
-# Print the numer of faces trained and end program
+# Save the model to trainer/trainer.yml
+recognizer.write('trainer/trainer.yml') # recognizer.save() works on Mac, but not on Pi
 print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
