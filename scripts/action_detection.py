Fixed frame rate, face size scaling

scripts/action_detection.py
@@ -1,20 +1,28 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import sys
+from camera_reader import CameraReader
 
 from keras.models import load_model
 
-face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
-
-video_capture = cv2.VideoCapture(0)
+face_cascade = cv.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 model = load_model('keypoints_model.h5')
+
+cv.startWindowThread()
+cv.namedWindow('mirror')
+
+camera = CameraReader()
+
+camera.begin_reading()
 
 while True:
     # Capture frame-by-frame
-    ret, frame = video_capture.read()
+    frame = camera.get_frame()
+    
+    if frame is None: # Wait for next frame
+        continue
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
     min_size = (int(0.05 * frame.shape[1]), int(0.05 * frame.shape[0]))
     faces = face_cascade.detectMultiScale(
@@ -26,11 +34,11 @@ while True:
 
     for (x,y,w,h) in faces:
         # Add a red bounding box to the detections image
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 3)
+        cv.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 3)
         bgr_crop = frame[y:y+h, x:x+w]
         orig_shape_crop = bgr_crop.shape
-        gray_crop = cv2.cvtColor(bgr_crop, cv2.COLOR_BGR2GRAY)
-        resize_gray_crop = cv2.resize(gray_crop, (96, 96)) / 255
+        gray_crop = cv.cvtColor(bgr_crop, cv.COLOR_BGR2GRAY)
+        resize_gray_crop = cv.resize(gray_crop, (96, 96)) / 255
         landmarks = np.squeeze(model.predict(
             np.expand_dims(np.expand_dims(resize_gray_crop, axis=-1), axis=0)))
 
@@ -38,14 +46,14 @@ while True:
         for i in range(0, len(landmarks), 2):
             xp = int((landmarks[i] * 48 + 48) * orig_shape_crop[0] / 96. + x)
             yp = int((landmarks[i+1] * 48 + 48) * orig_shape_crop[1] / 96. + y)
-            cv2.circle(frame, (xp, yp), 2, (0, 0, 255), 5)
+            cv.circle(frame, (xp, yp), 2, (0, 0, 255), 5)
 
     # Display the resulting frame
-    cv2.imshow('Video', frame)
+    cv.imshow('mirror', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
+camera.end_reading()
+cv.destroyAllWindows()

scripts/face.py
@@ -24,54 +24,75 @@ class Face:
         frame_width, frame_height = frame_dim
         (fx, fy, fw, fh) = face_rect
 
-        # EYE CASCADE METHOD
-        def cascade():
-            top_offset = int(fh * 0.2)
-            height_offset = int(fh * 0.5)
+        # # EYE CASCADE METHOD
+        # def cascade():
+        #     top_offset = int(fh * 0.2)
+        #     height_offset = int(fh * 0.5)
 
-            cropped_frame = face_img[top_offset:height_offset, :]
+        #     cropped_frame = face_img[top_offset:height_offset, :]
 
-            eyes = eye_cascade.detectMultiScale(cropped_frame)
+        #     eyes = eye_cascade.detectMultiScale(cropped_frame)
 
-            z = None
-            if len(eyes) == 2:
-                left_eye  = eyes[0] if eyes[0][0] > eyes[1][0] else eyes[1]
-                right_eye = eyes[0] if eyes[0][0] < eyes[1][0] else eyes[1]
+        #     z = None
+        #     if len(eyes) == 2:
+        #         left_eye  = eyes[0] if eyes[0][0] > eyes[1][0] else eyes[1]
+        #         right_eye = eyes[0] if eyes[0][0] < eyes[1][0] else eyes[1]
 
-                (lx, ly, lw, lh) = left_eye
-                (rx, ry, rw, rh) = right_eye
+        #         (lx, ly, lw, lh) = left_eye
+        #         (rx, ry, rw, rh) = right_eye
 
-                z = 1 - (((lx + lw / 2) - (rx + rw / 2)) / PIXELS_AT_STANDARD_DIST)
-            return z # z = 0 @ when user is at standard distance
+        #         z = 1 - (((lx + lw / 2) - (rx + rw / 2)) / PIXELS_AT_STANDARD_DIST)
+        #     return z # z = 0 @ when user is at standard distance
 
-        # TRAINED KEY FEATURE METHOD
-        def key_feature():
-            resize_gray_crop = cv.resize(face_img, (96, 96)) / 255
-            landmarks = np.squeeze(model.predict(
-                np.expand_dims(np.expand_dims(resize_gray_crop, axis=-1), axis=0)))
-            (rx, ry, lx, ly) = landmarks[0:4]
+        # # TRAINED KEY FEATURE METHOD
+        # def key_feature():
+        #     resize_gray_crop = cv.resize(face_img, (96, 96)) / 255
+        #     landmarks = np.squeeze(model.predict(
+        #         np.expand_dims(np.expand_dims(resize_gray_crop, axis=-1), axis=0)))
+        #     (rx, ry, lx, ly) = landmarks[0:4]
             
-            # 500 is around the closest I could get to the mirror before it stopped detecting my face
-            # 100 just felt like a good round number
-            # Multiply by width since rx and lx come from a scaled image
-            z = (500 - (rx - lx) * fw) / 50
+        #     # 500 is around the closest I could get to the mirror before it stopped detecting my face
+        #     # 100 just felt like a good round number
+        #     # Multiply by width since rx and lx come from a scaled image
+        #     z = (500 - (rx - lx) * fw) / 50
 
-            # Center x, y of face
-            _cx = (rx + lx) / 2
-            _cy = (ry + ly) / 2
-            cx = int((_cx * 48 + 48) * fw / 96. + fx)
-            cy = int((_cy * 48 + 48) * fh / 96. + fy)
+        #     # Center x, y of face
+        #     _cx = (rx + lx) / 2
+        #     _cy = (ry + ly) / 2
+        #     cx = int((_cx * 48 + 48) * fw / 96. + fx)
+        #     cy = int((_cy * 48 + 48) * fh / 96. + fy)
 
-            # 300 is to make a movement along the x/y axes feel similar to a movement along the z axis
-            # Dividing by 100 just didn't seem like enough on these values (everything is arbitary)
-            x =  (cx - frame_width / 2) / 300 * z
-            y = -(cy - frame_height / 2) / 300 * z
+        #     # 300 is to make a movement along the x/y axes feel similar to a movement along the z axis
+        #     # Dividing by 100 just didn't seem like enough on these values (everything is arbitary)
+        #     x =  (cx - frame_width / 2) / 300 * z
+        #     y = -(cy - frame_height / 2) / 300 * z
 
-            return Position(x, y, z)
+        #     return Position(x, y, z)
 
-        self.measured_position = key_feature()
+        # Normalized face size, independant of frame size scaling
+        # Faces are typically taller than they are wide, so we'll probably hit fh > frame_height first
+        face_size = fh / frame_height
+
+        z = (1.0 - face_size) * 10
+
+        # Center x, y of face
+        cx = fx + fw / 2
+        cy = fy + fh / 2
+
+        x =  (cx - frame_width / 2) / frame_height * z
+        y = -(cy - frame_height / 2) / frame_height * z
+
+        x = round(x, 2)
+        y = round(y, 2)
+        z = round(z, 2)
+
+        current_position = Position(x, y, z)
+
+        self.predicted_position = self.measured_position.update(current_position, 0.7)
+
+        self.measured_position = current_position
         self.measured_size = (int(fw), int(fh))
-        self.filtered_position.observe(time.time(), self.measured_position)
+        # self.filtered_position.observe(time.time(), self.measured_position)
 
     def get_distance_color(self, filter=False):
         if filter:

scripts/mirror_viz.py
@@ -33,7 +33,7 @@ def update_face(image, min_size):
         id, confidence = recognizer.predict(face_img)
 
         if confidence >= 100: # Unknown face
-           return updated_face_id
+            id = 0
 
         detected_user = users[id]
         detected_user.observe((image.shape[1], image.shape[0]), detected_face, face_img)
@@ -50,7 +50,7 @@ def render_ui(user, user_name, screen):
     filtered_color = user.get_distance_color(filter=True)
 
     # calculate perspective (assuming camera is center of mirror)
-    (x, y, z) = user.filtered_position.value()
+    (x, y, z) = user.predicted_position.value()
 
     # The mirror
     cv.line(
@@ -69,16 +69,12 @@ def main_loop(camera, mirror_mode):
     if frame is None: # Wait for next frame
         return
 
-    image_width, image_height = camera.get_img_dimensions()
+    gray = frame
+    frame = np.zeros((720, 1280, 3)) # Blank color image
+
     # Define min window size to be recognized as a face
+    image_width, image_height = camera.get_img_dimensions()
     min_face_size = (int(0.05 * image_width), int(0.05 * image_height))
-
-    if mirror_mode:
-        gray = frame
-        frame = np.zeros((image_height, image_width, 3)) # Blank color image
-    else:
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
     user_id = update_face(gray, min_face_size)
 
     if user_id is None: