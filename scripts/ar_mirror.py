66d810a85400f432e6e8cb6ac1006107817170a8
Robert Gerdisch <robert.gerdisch@yale.edu>
2018-12-02 18:59:05 -0500

    Started ar mirror

---
+++ scripts/ar_mirror.py
@@ -0,0 +1,37 @@
+import numpy as np
+import cv2 as cv
+from camera_reader import CameraReader
+from ui_render import render_ui
+
+RENDER_SIZE = (640, 400)
+SCREEN_SIZE = (1280, 800)
+
+def main_loop(camera):
+    frame = camera.get_frame()
+    
+    if frame is None: # Wait for next frame
+        return
+
+    screen = np.zeros((RENDER_SIZE[1], RENDER_SIZE[0], 3), dtype=np.uint8)  
+    render_ui(screen)
+
+    screen = cv.resize(screen, SCREEN_SIZE, interpolation=0)
+
+    cv.imshow('image', screen)
+
+if __name__ == "__main__":
+    cv.startWindowThread()
+    cv.namedWindow('image')
+    cv.setWindowProperty('image', 0, 1)
+
+    camera = CameraReader(True)
+
+    camera.begin_reading()
+
+    while True:
+        main_loop(camera)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    camera.end_reading()
+    cv.destroyAllWindows()
---
+++ scripts/ui_render.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+from time import gmtime, strftime
+
+def render_ui(screen):
+    screen_height, screen_width, _ = screen.shape
+    render_clock(screen, 10, 100)
+
+def render_clock(screen, x, y):
+    date_time = strftime("%a, %d %b %Y", gmtime())
+    clock_time = strftime("%H:%M:%S", gmtime())
+
+    cv.putText(screen, clock_time,
+        (x, y), cv.FONT_HERSHEY_SIMPLEX,
+        0.8, (0, 255, 0), 2)
+
+    cv.putText(screen, date_time,
+        (x, y + 20), cv.FONT_HERSHEY_SIMPLEX,
+        0.8, (0, 255, 0), 2)
