924b971cb089b5fd388b4f0ea60e37889f4618c9
Robert Gerdisch <robert.gerdisch@yale.edu>
2018-12-02 22:53:52 -0500

    Fixed action detector

--- scripts/action_detection.py
+++ scripts/action_detection.py
@@ -3,66 +3,55 @@ import time
 import collections
 
 action_cooldown_time = 1 # second
+action_time_threshold = 0.5 # second
 
 class ActionDetector:
     def __init__(self, callback):
-        self._clear_histories()
         self.callback = callback
-        self.px_hist = collections.deque([0], 3)
-        self.py_hist = collections.deque([0], 3)
-        self.position = (0, 0)
-        self.last_time = None
+        self.px_hist = collections.deque([0], 2)
+        self.py_hist = collections.deque([0], 2)
+        self.last_seen_left = 0
+        self.last_seen_right = 0
+        self.last_seen_up = 0
+        self.last_seen_down = 0
         self.last_action_time = 0
-
-    def _clear_histories(self):
-        self.vx_hist = collections.deque([0], 10)
-        self.vy_hist = collections.deque([0], 10)
 
     def update(self, position):
         current_time = time.time()
 
         self.px_hist.append(position[0]); self.py_hist.append(position[1])
 
-        if self.last_time is None:
-            self.last_time = current_time
-            return
-
-        elapsed = current_time - self.last_time
-        self.last_time = current_time
-
-        lpx, lpy = self.position # last px, py
-        px = np.mean(self.px_hist)
-        py = np.mean(self.py_hist)
+        px = np.mean(self.px_hist) * 100
+        py = (np.mean(self.py_hist) - 0.2) * 100
         self.position = (px, py)
 
-        vx = (px - lpx) * elapsed * 50
-        vy = (py - lpy) * elapsed * 100
-        self.vx_hist.append(vx); self.vy_hist.append(vy)
+        if px >= 6:
+            self.last_seen_right = current_time
+        elif px <= -6:
+            self.last_seen_left = current_time
 
-        stdv_vx = np.std(self.vx_hist) * 100
-        mean_vx = np.mean(self.vx_hist) * 100
-        stdv_vy = np.std(self.vy_hist) * 100
-        mean_vy = np.mean(self.vy_hist) * 100
+        if py <= -1:
+            self.last_seen_up = current_time
+        elif py >= 8:
+            self.last_seen_down = current_time        
+
+        self.last_seen_up    = self.last_seen_up    if current_time - self.last_seen_up    < action_time_threshold else 0
+        self.last_seen_down  = self.last_seen_down  if current_time - self.last_seen_down  < action_time_threshold else 0
+        self.last_seen_left  = self.last_seen_left  if current_time - self.last_seen_left  < action_time_threshold else 0
+        self.last_seen_right = self.last_seen_right if current_time - self.last_seen_right < action_time_threshold else 0
 
         action = None
 
-        if stdv_vx > 50 and abs(mean_vx) < 100:
+        if self.last_seen_left != 0 and self.last_seen_right != 0:
             action = "shake"
             
-        elif stdv_vy > 35 and abs(mean_vy) < 100:
+        elif self.last_seen_up != 0 and self.last_seen_down != 0:
             action = "nod"
             
         if action is not None and current_time - self.last_action_time > action_cooldown_time:
             self.last_action_time = current_time
-            self._clear_histories()
             self.callback(action)
 
-
+    # For debugging
     def get_position(self):
         return self.position
-
-    def get_velocity(self):
-        return np.mean(self.vx_hist), np.mean(self.vy_hist)
-
-    def get_velocity_stdev(self):
-        return np.std(self.vx_hist) * 100, np.std(self.vy_hist) * 100
